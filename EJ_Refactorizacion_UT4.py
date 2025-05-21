1c8a17333fb82dda5c3417447a8257118c8fb20b
Valennq12 <valenlovecrafts@gmail.com>
2025-05-21 17:44:31 +0200

    5.INVOCAMOS FUNCIONES ESTATICAS EN LA FUNCION MAIN . PREGUNTAMOS QUE TIPO DE RECETA QUIERE HACER PARA EVITAR DUPLICADOS EN CODIGO.

--- EJ_Refactorizacion_UT4.py
+++ EJ_Refactorizacion_UT4.py
@@ -16,6 +16,7 @@ class recetaVegetariana(receta):
 
 
 
+
 # Clase para recetas no vegetarianas
 class recetaNoVegetariana(receta):
     lista_recetas = []
@@ -25,15 +26,16 @@ class recetaNoVegetariana(receta):
 # Clase con utilidades del restaurante
 class utilidades:
     @staticmethod
-    def mostrar(receta):
-        print(f'{receta.nombre}')
+    def mostrar(lista_recetas):
+        for receta in lista_recetas:
+            print(f'{receta.nombre}')
 
-        print("Ingredientes:")
-        for ingrediente in receta.ingredientes:
-            print(f"- {ingrediente}")
-        print("Pasos:")
-        for paso in receta.pasos:
-            print(f"{paso}")
+            print("Ingredientes:")
+            for ingrediente in receta.ingredientes:
+                print(f"- {ingrediente}")
+            print("Pasos:")
+            for paso in receta.pasos:
+                print(f"{paso}")
 
     @staticmethod
     def imprimir_receta(receta):
@@ -48,30 +50,62 @@ class utilidades:
 
     @staticmethod
     def crear_receta():
+        
         nombre_receta = input('Nombre receta: ')
         ingrediente = 'si'
         lista_ingredientes = []
         while ingrediente != 'no':
-            ingrediente = input('Dime un ingrediente. SI NO QUIERES AÑADIR MÁS ESCRIBE NO: ').lower()
-            lista_ingredientes.append(ingrediente)
+            if ingrediente != 'no':
+                ingrediente = input('Dime un INGREDIENTE. SI NO QUIERES AÑADIR MÁS ESCRIBE NO: ').lower()
+                lista_ingredientes.append(ingrediente)
+            else:
+                break
+            
+            
             
         paso = 'si'
         lista_pasos = []
         while paso != 'no':
-            paso = input('Dime un ingrediente. SI NO QUIERES AÑADIR MÁS ESCRIBE NO: ').lower()
-            lista_pasos.append(paso)
+            if paso != 'no':
+                paso = input('Dime un PASO. SI NO QUIERES AÑADIR MÁS ESCRIBE NO: ').lower()
+                lista_pasos.append(paso)
+            else:
+                break
 
         return nombre_receta,lista_ingredientes,lista_pasos
 
-
-            
-
-
-
 # Función principal
 def Main():
+    print('VAMOS A CREAR UNA RECETA')
+    while True:
+        opcion = input('RECETA VEGETARIANA (SI|NO)?: ').lower()
+        
+        if opcion == 'si':
+            nombre_receta,lista_ingredientes,lista_pasos = utilidades.crear_receta()
+            receta = recetaVegetariana(nombre_receta,lista_ingredientes,lista_pasos)
+            recetaVegetariana.lista_recetas.append(receta)
+            print()
+            
+
+        elif opcion == 'no':
+            nombre_receta,lista_ingredientes,lista_pasos = utilidades.crear_receta()
+            receta = recetaNoVegetariana(nombre_receta,lista_ingredientes,lista_pasos)
+            recetaNoVegetariana.lista_recetas.append(receta)
+            print()
+            
+        elif opcion != 'si' or opcion != 'no':
+            print('No se ha especificado el tipo de receta. OPCIONES: SI O NO')
+            break
+        
+        print()
+        print('RECETAS VEGETARIANAS')
+        utilidades.mostrar(recetaVegetariana.lista_recetas)
+        print('RECETAS NO VEGETARIANAS')
+        print()
+        utilidades.mostrar(recetaNoVegetariana.lista_recetas)
+        print()
+        
     
-   pass
 
 
 # Ejecutar el programa
